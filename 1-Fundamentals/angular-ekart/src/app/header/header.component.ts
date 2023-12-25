a1a8e556f2b4988e0974906a5ca4c4bdb3c56f2c
Naveen Manjappa <NManjappa@manh.com>
2023-12-25 16:45:20 +0000

    Added CSS to the component using styles url

--- 1-Fundamentals/angular-ekart/src/app/header/header.component.ts
+++ 1-Fundamentals/angular-ekart/src/app/header/header.component.ts
@@ -3,7 +3,12 @@ import { Component } from "@angular/core";
 @Component({
     selector:'app-header',
     templateUrl:'./header.component.html',
-    styles:['a{text-decoration:none;margin:0 10px}','button{padding: 10px 20px;}','.ekart-header{width:100%;height:2700px}']
+    // styles:[
+    //     'a{text-decoration:none;margin:0 10px}',
+    //     'button{padding: 10px 20px;}',
+    //     '.ekart-header{width:100%;height:2700px}'
+    // ]
+    styleUrls:['./header.component.css']
 })
 export class HeaderComponent{
 

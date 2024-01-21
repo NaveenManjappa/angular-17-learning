0a519b5e4fb8f9aa538f6b8be766993c547810f5
Naveen Manjappa <NManjappa@manh.com>
2024-01-21 17:01:15 +0000

    Added Event binding changes

--- 1-Fundamentals/angular-ekart/src/app/product-list/product-list.component.ts
+++ 1-Fundamentals/angular-ekart/src/app/product-list/product-list.component.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
-name= 'John Doe'
+//name= 'John Smith'
+addToCart:number=0;
 product = {
   name: 'iPhone 15',
   price: 789,
@@ -21,4 +22,20 @@ product = {
 getDiscountedPrice(){
   return this.product.price-(this.product.price*this.product.discount/100);
 }
+
+onNameChange(event:any){
+  //console.log(event.target.value);
+  //this.name=event.target.value;
+}
+
+incrementValue(){
+  if(this.addToCart < this.product.inStock)
+    this.addToCart++;
+}
+
+decrementValue(){
+  if(this.addToCart > 0)
+    this.addToCart--;
+}
+
 }

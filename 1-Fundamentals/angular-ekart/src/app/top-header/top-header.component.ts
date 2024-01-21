2906a7db485993464fa9992c4e446ae38bc50eed
Naveen Manjappa <NManjappa@manh.com>
2024-01-21 08:12:17 +0000

    Added different component selectors

--- 1-Fundamentals/angular-ekart/src/app/top-header/top-header.component.ts
+++ 1-Fundamentals/angular-ekart/src/app/top-header/top-header.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //  HTML Attribute selector
+  // selector: '[top-header]',
+ //  CSS class selector
+  // selector: '.top-header',
+  //  CSS id selector
+  // selector: '#top-header',
   selector: 'top-header',
   templateUrl: './top-header.component.html',
   styleUrl: './top-header.component.css'

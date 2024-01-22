fd26fbd1b7e1978f5a8b71a12e44a947af7b1d56
Naveen Manjappa <NManjappa@manh.com>
2024-01-22 19:02:49 +0000

    ngFor Directive changes

--- 1-Fundamentals/angular-ekart/src/app/product-list/product-list.component.ts
+++ 1-Fundamentals/angular-ekart/src/app/product-list/product-list.component.ts
@@ -8,34 +8,35 @@ import { Component } from '@angular/core';
 export class ProductListComponent {
 
 //name= 'John Smith'
-addToCart:number=0;
-product = {
-  name: 'iPhone 15',
-  price: 789,
-  color: 'Mate Black',
-  discount:8.5,
-  inStock:10,
-  pImage:'/assets/images/iPhone.png'
-}
+// addToCart:number=0;
+// product = {
+//   name: 'iPhone 15',
+//   price: 789,
+//   color: 'Mate Black',
+//   discount:8.5,
+//   inStock:10,
+//   pImage:'/assets/images/iPhone.png'
+// }
 
 
-getDiscountedPrice(){
-  return this.product.price-(this.product.price*this.product.discount/100);
-}
+// getDiscountedPrice(){
+//   return this.product.price-(this.product.price*this.product.discount/100);
+// }
 
-onNameChange(event:any){
-  //console.log(event.target.value);
-  //this.name=event.target.value;
-}
+// onNameChange(event:any){
+  
+// }
 
-incrementValue(){
-  if(this.addToCart < this.product.inStock)
-    this.addToCart++;
-}
+// incrementValue(){
+//   if(this.addToCart < this.product.inStock)
+//     this.addToCart++;
+// }
 
-decrementValue(){
-  if(this.addToCart > 0)
-    this.addToCart--;
-}
+// decrementValue(){
+//   if(this.addToCart > 0)
+//     this.addToCart--;
+// }
+
+names: string[] = ['Mark','Jake','Blake','Ted']
 
 }

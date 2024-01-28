ec06ecf30555c4659dc10d2581321b2de3fa791b
Naveen Manjappa <NManjappa@manh.com>
2024-01-28 09:27:43 +0000

    Deleted unwanted code and added the required initial changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { DemoComponent } from './demo/demo.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,DemoComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

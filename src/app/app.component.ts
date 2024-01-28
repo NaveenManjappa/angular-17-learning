d940918e1f87bf40b1873a5151829617010a1f09
Naveen Manjappa <NManjappa@manh.com>
2024-01-28 14:55:27 +0000

    Creating clone of ngClass custom directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ClassDirective } from './CustomDirectives/class.directive';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,ClassDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

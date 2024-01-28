d76aef234040a9959a28d034f9407144ecbb34c8
Naveen Manjappa <NManjappa@manh.com>
2024-01-28 08:48:34 +0000

    Using properties on custom directives

--- 1-Fundamentals/angular-ekart/src/app/CustomDirectives/SetBackground.directive.ts
+++ 1-Fundamentals/angular-ekart/src/app/CustomDirectives/SetBackground.directive.ts
@@ -1,10 +1,15 @@
-import {Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import {Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
     selector:'[setBackground]'
 })
 
 export class SetBackground implements OnInit{
+
+    // @Input('setBackground') backColor:string='#36454F';
+    // @Input() textColor:string='white';
+
+        @Input('setBackground') changeTextAndBackColor: { backColor:string,textColor:string};
 
         constructor(private element:ElementRef,private renderer:Renderer2){
             
@@ -14,7 +19,7 @@ export class SetBackground implements OnInit{
             // this.element.nativeElement.style.backgroundColor='#36454F';
             // this.element.nativeElement.style.color='white';
 
-            this.renderer.setStyle(this.element.nativeElement,'backgroundColor','#36454F');
-            this.renderer.setStyle(this.element.nativeElement,'color','white');
+            this.renderer.setStyle(this.element.nativeElement,'backgroundColor',this.changeTextAndBackColor.backColor);
+            this.renderer.setStyle(this.element.nativeElement,'color',this.changeTextAndBackColor.textColor);
         }
 }

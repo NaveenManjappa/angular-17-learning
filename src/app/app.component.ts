6b06317eeaddfa41d48e516187016112ae096cd1
Naveen Manjappa <NManjappa@manh.com>
2024-02-01 18:01:12 +0000

    Observable using RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 
 
@@ -13,7 +14,27 @@ import { RouterOutlet } from '@angular/router';
   
 })
 export class AppComponent {
+title='angular-observables';
+data:any[]=[];
 
-
+//1.Creata an Observable
+  myObservable=new Observable((observer)=>{
+    setTimeout(()=>observer.next(1),1000);
+    setTimeout(()=>observer.next(2),2000);
+    setTimeout(()=>observer.next(3),3000);
+    setTimeout(()=>observer.next(4),4000);
+    setTimeout(()=>observer.next(5),5000);
+  });
   
+ 
+  GetAsyncData(){
+
+     //2.Observer
+     //next,error,complete
+    this.myObservable.subscribe(
+      (val:any)=>{
+        this.data.push(val);
+      }
+    );
+  }
 }

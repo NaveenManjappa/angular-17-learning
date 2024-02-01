ddf9eefa20a8e11f0327226820021fbdc66a2bd2
Naveen Manjappa <NManjappa@manh.com>
2024-02-01 17:44:57 +0000

    Initial project cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
   
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,15 +2,13 @@ import { ApplicationConfig, InjectionToken } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { SubscribeService } from './Services/subscribe.service';
-import { UserService } from './Services/user.service';
-import { LoggerService } from './Services/logger.service';
 
-export const USER_TOKEN=new InjectionToken<UserService>('User_Service');
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [    
-    {provide:USER_TOKEN,useClass:UserService},    
+    
     provideRouter(routes)
   ]
 };
